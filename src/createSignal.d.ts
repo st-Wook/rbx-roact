3f6b800b6641dd4f469338800d9dd384a8ebc472
supertree-wook <phk09242@supertree.co>
2023-10-27 17:31:13 +0900

    Export createSignal

---
+++ src/createSignal.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="@rbxts/types" />
+
+declare function createSignal<T extends void>(): {
+	subscribe: (callback: () => void) => void;
+	fire: () => void;
+};
+declare function createSignal<T extends ReadonlyArray<any>>(): {
+	subscribe: (callback: (...args: T) => void) => void;
+	fire: (...args: T) => void;
+};
+declare function createSignal<T>(): {
+	subscribe: (callback: (value: T) => void) => void;
+	fire: (value: T) => void;
+};
+
+export = createSignal;
--- src/init.lua
+++ src/init.lua
@@ -22,6 +22,7 @@ local Roact = strict({
 	None = require(script.None),
 	Portal = require(script.Portal),
 	createRef = require(script.createRef),
+	createSignal = require(script.createSignal),
 	forwardRef = require(script.forwardRef),
 	createBinding = Binding.create,
 	joinBindings = Binding.join,
