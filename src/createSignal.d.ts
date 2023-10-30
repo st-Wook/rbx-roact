1097dba464fb4a9b6dd8ee1faa268b5099265f29
supertree-wook <phk09242@supertree.co>
2023-10-30 11:39:37 +0900

    Update createSignal.d.ts

--- src/createSignal.d.ts
+++ src/createSignal.d.ts
@@ -1,15 +1,17 @@
 /// <reference types="@rbxts/types" />
 
+type Disconnect = () => void;
+
 declare function createSignal(): {
-	subscribe: (callback: () => void) => void;
+	subscribe: (callback: () => void) => Disconnect;
 	fire: () => void;
 };
 declare function createSignal<T extends ReadonlyArray<any>>(): {
-	subscribe: (callback: (...args: T) => void) => void;
+	subscribe: (callback: (...args: T) => void) => Disconnect;
 	fire: (...args: T) => void;
 };
 declare function createSignal<T>(): {
-	subscribe: (callback: (value: T) => void) => void;
+	subscribe: (callback: (value: T) => void) => Disconnect;
 	fire: (value: T) => void;
 };
 
